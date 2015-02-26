cfd2f2fe73165de9e2ab2f126e0affa607cf218b
Scott Hill <scott289@gmail.com>
2015-02-26 15:12:44 -0800

    trim couple words

--- horoscope/dirtywords.py
+++ horoscope/dirtywords.py
@@ -1,13 +1,11 @@
 avoid_list = [
     "poor people",
     "mamallian flesh",
-    "white people",
     "foreigners",
     "oral sex",
     "bullshit artists",
     "cats that look like Jesus",
     "anarcho-syndicalists",
-    "athesists",
     "Christians",
     "Mormons",
     "hard drugs",
