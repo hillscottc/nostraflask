51f1644ac6b425be5adeb829a04351c26cc6beb3
Scott Hill <scott289@gmail.com>
2015-05-21 15:40:07 -0700

    minor refactor

--- nostra.py
+++ nostra.py
@@ -1,7 +1,4 @@
-import os
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
-
+from flask import Flask, jsonify, render_template
 from horoscope import horoscope
 
 SECRET_KEY = 'development key'
@@ -24,11 +21,17 @@ def about():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)    
+    return render_template('hello.html', name=name)
 
-@app.route('/gen')
+
+@app.route('/gen', methods=['GET', 'POST'])
 def gen():
-    return horoscope.generate()   
+    return horoscope.generate()
+
+
+@app.route('/fortune', methods=['GET', 'POST'])
+def fortune():
+    return jsonify({'fortune': horoscope.generate()})
 
 if __name__ == '__main__':
     app.run(debug=True)
