950ca430856297d59d2d8e65d29f03c520587c53
Scott Hill <scott289@gmail.com>
2015-12-21 06:31:02 -0800

    app to application

--- webapp/application.py
+++ webapp/application.py
@@ -2,41 +2,41 @@ import os
 from flask import Flask, render_template, jsonify
 from horoscope import horoscope
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # configuration
 DEBUG = True
-app.config.from_object(__name__)
+application.config.from_object(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     provider = str(os.environ.get('PROVIDER', 'world'))
     return 'Hello '+provider+'!'
 
 
-@app.route('/home')
+@application.route('/home')
 def home():
     return render_template('home.html')
 
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@application.route('/hello/')
+@application.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/gen', methods=['GET', 'POST'])
+@application.route('/gen', methods=['GET', 'POST'])
 def gen():
     return horoscope.generate()
 
 
-@app.route('/fortune', methods=['GET', 'POST'])
+@application.route('/fortune', methods=['GET', 'POST'])
 def fortune():
     return jsonify({'fortune': horoscope.generate()})
 
@@ -44,4 +44,4 @@ def fortune():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
